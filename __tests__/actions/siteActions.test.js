fef62e1907c30adb232ac31ab70539af0b4c11da
joel-ace <andeladeveloper@Andelas-MacBook-Pro.local>
2017-06-17 13:05:40 +0100

    update to Package.json filp

--- __tests__/actions/siteActions.test.js
+++ __tests__/actions/siteActions.test.js
@@ -1,20 +1,20 @@
 import dispatcher from '../../src/dispatcher/dispatcher';
-import * as newsActions from '../../src/actions/siteActions';
+import * as siteActions from '../../src/actions/siteActions';
 
 // Mock the dispatcher
 jest.mock('../../src/dispatcher/dispatcher');
 
 // Mock the utils class to get getSources and getArticle
-const Utils = jest.mock('../../src/utils.js', () => ({
-  getSources: () => Promise.resolve('getPromise'),
-  getArticle: () => Promise.resolve('getPromise'),
-}));
+jest.mock('../../src/utils.js');
 
 const dispatchSpy = jest.spyOn(dispatcher, 'dispatch');
 
 describe('Actions', () => {
   test('should call Utils.getSources() on fetchSources', () => {
-    newsActions.fetchSources();
-    expect((Utils.getSources()).mock.calls.length).toBe(1);
+    siteActions.fetchSources();
+    expect((Utils.getSources()).mock.calls.length).toHaveBeenCalled();
+  });
+  test('should have a type of "GET_ARTICLES"', () => {
+    expect(siteActions.fetchSources('cnn', 'top').type).toEqual('GET_ARTICLES');
   });
 });
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname));
+// Set the public folder as source for rendering static file
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+// Send all requests to index.html
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port);
+// Tell server to listen on the port set by the environment or use port 3000
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
